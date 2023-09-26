0033702a451414373407b4619be27a0800beb8df
sanchezcarlosjr <24639141+sanchezcarlosjr@users.noreply.github.com>
2023-09-26 09:44:54 -0700

    feat: improve server

--- src/breast_cancer_toolkit/server.py
+++ src/breast_cancer_toolkit/server.py
@@ -1,7 +1,8 @@
 import gradio as gr
 from breast_cancer_toolkit.pipeline import pipeline
-from random import random
+from random import random, sample
 import numpy as np
+import glob
 
 def softmax_stable(x):
     return(np.exp(x - np.max(x)) / np.exp(x - np.max(x)).sum())
@@ -14,6 +15,14 @@ def execute_pipeline(predict):
     return fn
 
 def launch_server():
+    files = [*glob.glob("**/*.dcm", recursive=True),
+             *glob.glob("**/*.tif", recursive=True),
+             *glob.glob("**/*.tiff", recursive=True),
+             *glob.glob("**/*.npy", recursive=True), 
+             *glob.glob("**/*.png", recursive=True), 
+             *glob.glob("**/*.jpg", recursive=True), 
+             *glob.glob("**/*.jpeg", recursive=True)
+            ]
     gradio_interface = gr.Interface(
       fn = execute_pipeline(pipeline()),
       title = "UABC Breast Cancer Toolkit",
@@ -24,7 +33,8 @@ def launch_server():
       outputs = [
          "image",
          gr.Label(num_top_classes=4)
-      ]
+      ],
+      examples=sample(files, min(5, len(files)))
     )
-    gradio_interface.launch()
+    gradio_interface.launch(share=True)
 
